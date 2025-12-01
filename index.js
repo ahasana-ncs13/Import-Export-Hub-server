19eaff3bb163b345ff6e82fb78890270c600307d
ahasana-ncs13 <ahasanancs0485@gmail.com>
2025-12-01 23:17:13 +0600

    added all products API ,latest products API ,slider API ,product details API

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const cors = require('cors');
 const app = express()
@@ -32,10 +32,36 @@ async function run() {
 
     const importExportDB = client.db("ImportExportHub");
     const productCollection = importExportDB.collection("productinfo");
+    const sliderCollection = importExportDB.collection("slider");
 
+    // all products API 
     app.get('/productinfo',async(req,res)=>{
           const cursor = productCollection.find({});
         const result = await cursor.toArray();
+        res.send(result)
+    })
+
+    // latest products API 
+    app.get('/latestproduct',async(req,res)=>{
+          const cursor = productCollection.find({}).sort({created_at:-1}).limit(6);
+        const result = await cursor.toArray();
+        res.send(result)
+    })
+
+    // slider API 
+    app.get('/slider',async(req,res)=>{
+          const cursor = sliderCollection.find({});
+        const result = await cursor.toArray();
+        res.send(result)
+    })
+
+    // product details API
+    app.get('/productinfo/:id',async(req,res)=>{
+        const id = req.params.id
+        console.log(id)
+        const query = {_id : new ObjectId(id)}
+         console.log(query)
+        const result = await productCollection.findOne(query);
         res.send(result)
     })
 
