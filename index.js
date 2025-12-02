35cee186e44b45d59bad23fc2149c8b260ab4802
ahasana-ncs13 <ahasanancs0485@gmail.com>
2025-12-03 00:12:25 +0600

    my exports api

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 3000;
-
 
 app.use(cors());
 app.use(express.json());
@@ -14,9 +12,7 @@ app.get("/", (req, res) => {
   res.send("Import Export Hub server is running");
 });
 
-
-const uri =
-  `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.j6dmigp.mongodb.net/?appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.j6dmigp.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -34,6 +30,7 @@ async function run() {
     const productCollection = importExportDB.collection("productinfo");
     const sliderCollection = importExportDB.collection("slider");
     const MyImportsCollection = importExportDB.collection("myimports");
+    const MyExportCollection = importExportDB.collection("myexports");
 
     // all products API
     app.get("/productinfo", async (req, res) => {
@@ -68,21 +65,80 @@ async function run() {
     });
 
     // create import product API
-
     app.post("/myimports/:id", async (req, res) => {
       const id = req.params.id;
       const { Quantity } = req.body;
-      const importProduct = req.body
-      
-      
-       await productCollection.updateOne(
-      { _id: new ObjectId(id) },
-      { $inc: { available_quantity : -Quantity } }
-    );
+      const importProduct = req.body;
 
-      const result = await MyImportsCollection.insertOne(
-        importProduct
+      await productCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $inc: { available_quantity: -Quantity } }
       );
+
+      const result = await MyImportsCollection.insertOne(importProduct);
+      res.send(result);
+    });
+
+    // myImports API
+    app.get("/myimports", async (req, res) => {
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.email = email;
+      }
+      const cursor = MyImportsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // myImports Delete API
+    app.delete("/myimports/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await MyImportsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Add Exports post API
+    // app.post('/productinfo',async(req,res)=>{
+    //     const exportsProduct = req.body
+    //     // const query = {_id : new ObjectId(id)}
+    //     const result= await productCollection.insertOne(exportsProduct)
+    //     res.send(result)
+    // })
+
+    // myexports API
+    // app.get('/myexports',async(req,res)=>{
+    //     const email = req.query.email
+    //     console.log(email)
+    //     const query ={}
+    //     if (email) {
+    //         query.email=email
+    //     }
+    //     const cursor = MyExportCollection.find(query);
+    //   const result = await cursor.toArray();
+    //   res.send(result);
+    // })
+
+    // MyExports post API
+    app.post("/myexports", async (req, res) => {
+      const exportsMyProducts = req.body;
+      const result = await MyExportCollection.insertOne(exportsMyProducts);
+      res.send(result);
+    });
+
+    // MyExports get API
+    app.get("/myexports", async (req, res) => {
+      const cursor = MyExportCollection.find({});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // MyExports delete API
+    app.delete("/myexports/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await MyExportCollection.deleteOne(query);
       res.send(result);
     });
 
